2f09c50b2d0f22142616318f41f1e0dc52cb1d69
janindu_wije <janinduwije21@gmail.com>
2025-07-13 16:37:37 +0530

    added view_tasks,update_task,delete_task and the main menu

--- task_manger_main.py
+++ task_manger_main.py
@@ -61,3 +61,80 @@ def add_task():
 
         save_tasks_to_file()
         more = input("Do you want to enter More Data? (y/n): ").lower()
+def view_tasks():
+    if len(tasks) == 0:
+        print("No tasks available")
+    else:
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}.\n Task Name = {task[0]}\n task Description = {task[1]}\n Priority = {task[2]}\n Due: {task[3]}")
+
+def update_task():
+    if len(tasks) == 0:
+        print("No tasks available")
+        return
+
+    view_tasks()
+    task_number = input("\nEnter task number to update: ")
+
+    try:
+        task_number = int(task_number)
+        task = tasks[task_number - 1]
+
+        task[0] = input("Enter new task name: ")
+        task[1] = input("Enter new task description: ")
+        priority = input("Enter new priority (High, Medium, Low): ").lower()
+        while priority not in ["high", "medium", "low"]:
+            print("Invalid priority! Try again.")
+            priority = input("Enter new priority (High, Medium, Low): ").lower()
+        task[2] = priority
+        task[3] = input("Enter new due date (YYYY-MM-DD): ")
+
+        print("Task updated successfully!")
+        save_tasks_to_file()
+    except IndexError:
+        print("Invalid task number!")
+
+def delete_task():
+    if len(tasks) == 0:
+        print("No tasks available")
+        return
+
+    view_tasks()
+    task_number = input("\nEnter task number to delete: ")
+
+    try:
+        task_number = int(task_number)
+        task = tasks[task_number - 1]
+        tasks.remove(task)
+        print("Task deleted successfully!")
+        save_tasks_to_file()
+    except IndexError:
+        print("Invalid task number!")
+
+
+if __name__ == "_main_":
+    load_tasks_from_file()
+    while True:
+        menu = """
+        |Task Manager Main Menu|
+        |----------------------|
+            |1. Add Task    |
+            |2. View Tasks  |
+            |3. Update Task |
+            |4. Delete Task |
+            |5. Exit        |
+        """
+        print(menu)
+        choice = input("Enter your choice (1-5): ")
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            view_tasks()
+        elif choice == "3":
+            update_task()
+        elif choice == "4":
+            delete_task()
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice! Please enter a number between 1-5")
