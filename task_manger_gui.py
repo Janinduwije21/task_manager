57de6a96b4a0918cdae343f3606ebbcb6b67d734
janindu_wije <janinduwije21@gmail.com>
2025-07-14 21:39:50 +0530

    added the fuction for the view tasks in gui

--- task_manger_gui.py
+++ task_manger_gui.py
@@ -4,3 +4,12 @@ import task_manager_main as core
 
 def gui_add_task():
     core.add_task()
+
+def gui_view_tasks():
+    core.load_tasks_from_file()
+    display = ""
+    for i, task in enumerate(core.tasks, 1):
+        display += f"{i}. {task[0]} | {task[1]} | {task[2].capitalize()} | {task[3]}\n"
+    if not display:
+        display = "No tasks available."
+    messagebox.showinfo("Task List", display)
